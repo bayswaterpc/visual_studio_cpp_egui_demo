9b627d575a334c9a11dc66c843e5b77265a9ffa8
BayBay <bayswaterpc@gmail.com>
2021-05-26 17:51:53 -0400

    Adding commented out attempt at putting app in background thread
    Causes runtime error so won't work :(

--- egui_demo_lib/src/lib.rs
+++ egui_demo_lib/src/lib.rs
@@ -7,8 +7,23 @@ pub extern fn start_egui_demo() {
         decorated: true,
         ..Default::default()
     };
-
-
     eframe::run_native(Box::new(app), options);
-    // println!("Why no reach?");
+    println!("Like run but graceful exit");
+
+    //thread '<unnamed>' panicked at 'Initializing the event loop outside of the main thread is a significant cross-platform compatibility hazard. If you really, absolutely need to create an EventLoop on a different thread, please use the `EventLoopExtWindows::new_any_thread` function.', C:\Users\jmw99\.cargo\registry\src\github.com-1ecc6299db9ec823\winit-0.24.0\src\platform_impl\windows\event_loop.rs:136:9
+    //note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+
+    //fails with runtime error
+    // let child = std::thread::spawn( move || {
+    //     let app = egui_demo_lib::WrapApp::default();
+    //     let options = eframe::NativeOptions {
+    //         // Let's show off that we support transparent windows
+    //         transparent: true,
+    //         decorated: true,
+    //         ..Default::default()
+    //     };
+    //     eframe::run_native_graceful(Box::new(app), options);
+    // });
+    // child.join();
+    //println!("Why no reach?");
 }
