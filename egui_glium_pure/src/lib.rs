901af1b8853ec31cbeedcf5f041856a7b6bb359e
Jacob <bayswaterpc@gmail.com>
2021-05-31 20:53:06 -0400

    Explicit Call to DestroyWindow Fixes issue

--- egui_glium_pure/src/lib.rs
+++ egui_glium_pure/src/lib.rs
@@ -1,6 +1,17 @@
 //! Example how to use pure `egui_glium` without [`epi`].
 use glium::glutin;
 use glium::glutin::platform::run_return::EventLoopExtRunReturn;
+use winapi::{
+    shared::windef::HWND,
+    um::{
+        winuser,
+    },
+};
+
+use glium::backend::glutin::glutin::platform::windows::WindowExtWindows;
+use egui_glium::EguiGlium;
+use glium::glutin::event_loop::ControlFlow;
+
 
 fn create_display(event_loop: &glutin::event_loop::EventLoop<()>) -> glium::Display {
     let window_builder = glutin::window::WindowBuilder::new()
@@ -20,99 +31,94 @@ fn create_display(event_loop: &glutin::event_loop::EventLoop<()>) -> glium::Disp
     glium::Display::new(window_builder, context_builder, &event_loop).unwrap()
 }
 
+fn redraw(egui : &mut egui_glium::EguiGlium, display: &glium::Display, control_flow: &mut ControlFlow){
+    egui.begin_frame(&display);
+
+    let mut quit = false;
+
+    egui::SidePanel::left("my_side_panel", 300.0).show(egui.ctx(), |ui| {
+        ui.heading("Hello World!");
+        if ui.button("Quit").clicked() {
+            quit = true;
+        }
+
+        egui::ComboBox::from_label("Version")
+            .width(150.0)
+            .selected_text("foo")
+            .show_ui(ui, |ui| {
+                egui::CollapsingHeader::new("Dev")
+                    .default_open(true)
+                    .show(ui, |ui| {
+                        ui.label("contents");
+                    });
+            });
+    });
+
+    let (needs_repaint, shapes) = egui.end_frame(&display);
+
+    *control_flow = if quit {
+        glutin::event_loop::ControlFlow::Exit
+    } else if needs_repaint {
+        display.gl_window().window().request_redraw();
+        glutin::event_loop::ControlFlow::Poll
+    } else {
+        glutin::event_loop::ControlFlow::Wait
+    };
+
+    {
+        use glium::Surface as _;
+        let mut target = display.draw();
+
+        let clear_color = egui::Rgba::from_rgb(0.1, 0.3, 0.2);
+        target.clear_color(
+            clear_color[0],
+            clear_color[1],
+            clear_color[2],
+            clear_color[3],
+        );
+
+        // draw things behind egui here
+
+        egui.paint(&display, &mut target, shapes);
+
+        // draw things on top of egui here
+
+        target.finish().unwrap();
+    }
+}
+
 pub fn egui_glium_pure_example() {
     let mut event_loop = glutin::event_loop::EventLoop::with_user_event();
     let display = create_display(&&event_loop);
 
     let mut egui = egui_glium::EguiGlium::new(&display);
+    let h_wnd = display.gl_window().window().hwnd() as HWND;
 
     event_loop.run_return(move |event, _, control_flow| {
-        let mut redraw = || {
-            egui.begin_frame(&display);
-
-            let mut quit = false;
-
-            egui::SidePanel::left("my_side_panel", 300.0).show(egui.ctx(), |ui| {
-                ui.heading("Hello World!");
-                if ui.button("Quit").clicked() {
-                    quit = true;
-                }
-
-                egui::ComboBox::from_label("Version")
-                    .width(150.0)
-                    .selected_text("foo")
-                    .show_ui(ui, |ui| {
-                        egui::CollapsingHeader::new("Dev")
-                            .default_open(true)
-                            .show(ui, |ui| {
-                                ui.label("contents");
-                            });
-                    });
-            });
-
-            let (needs_repaint, shapes) = egui.end_frame(&display);
-
-            *control_flow = if quit {
-                println!("glutin::event_loop::ControlFlow::Exit");
-                glutin::event_loop::ControlFlow::Exit
-            } else if needs_repaint {
-                display.gl_window().window().request_redraw();
-                glutin::event_loop::ControlFlow::Poll
-            } else {
-                glutin::event_loop::ControlFlow::Wait
-            };
-
-            {
-                use glium::Surface as _;
-                let mut target = display.draw();
-
-                let clear_color = egui::Rgba::from_rgb(0.1, 0.3, 0.2);
-                target.clear_color(
-                    clear_color[0],
-                    clear_color[1],
-                    clear_color[2],
-                    clear_color[3],
-                );
-
-                // draw things behind egui here
-
-                egui.paint(&display, &mut target, shapes);
-
-                // draw things on top of egui here
-
-                target.finish().unwrap();
-            }
-        };
 
         match event {
             // Platform-dependent event handlers to workaround a winit bug
             // See: https://github.com/rust-windowing/winit/issues/987
             // See: https://github.com/rust-windowing/winit/issues/1619
-            glutin::event::Event::RedrawEventsCleared if cfg!(windows) => redraw(),
-            glutin::event::Event::RedrawRequested(_) if !cfg!(windows) => redraw(),
+            glutin::event::Event::RedrawEventsCleared if cfg!(windows) => redraw(&mut egui, &display, control_flow),
+            glutin::event::Event::RedrawRequested(_) if !cfg!(windows) => redraw(&mut egui, &display, control_flow),
 
             glutin::event::Event::WindowEvent { event, .. } => {
                 egui.on_event(event, control_flow);
-                match control_flow {
-                    glutin::event_loop::ControlFlow::Exit => {
-                        println!("exiting");
-                        std::mem::drop(display.gl_window().window());
-                        println!("Dropped Winit Window");
-                        std::mem::drop(display.gl_window());
-                        println!("Dropped Gl Window")
-                        //std::mem::drop(display);
-                    }
-                    _ => {
-                        display.gl_window().window().request_redraw(); // TODO: ask egui if the events warrants a repaint instead
-                    }
-                }
+                display.gl_window().window().request_redraw(); // TODO: ask egui if the events warrants a repaint instead
             }
             _ => (),
         }
     });
-    //display.flush();
-    //display.gl;
-    println!("egui after event loop");
+
+    // std::mem::drop(display);
+    // std::mem::drop(egui);
+    // std::mem::drop(event_loop);
+
+    println!("Explicit call to display h_wnd");
+    unsafe {
+        winuser::DestroyWindow(h_wnd);
+    }
 }
 
 #[no_mangle]
